c7c3e060f0f23f6e88cdc10c32f634247a53c3cd
Dima <122263971+dmsprn@users.noreply.github.com>
2023-04-18 19:05:42 +0300

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,15 @@
-'use strict'
-
 // Button go start
-const $go = document.querySelector('#go');
-const $playerDamage = document.getElementById('playerDamage');
-const $rivalDamage = document.getElementById('rivalDamage');
+const $go = document.querySelector("#go");
+const $playerDamage = document.getElementById("playerDamage");
+const $rivalDamage = document.getElementById("rivalDamage");
 
-
-console.log($playerDamage)
+console.log($playerDamage);
 /// /////////////////////////////////////////////
 
 const reload = () => {
   location.reload();
 };
 
-/// ////////////////////////////////////////////
 const defaultSettings = {
   hp: 100,
   attack: 20,
@@ -21,9 +17,13 @@ const defaultSettings = {
 
 class Player {
   name = null;
+
   hp = 0;
+
   attack = 0;
+
   killed = false;
+
   constructor(name, settings = null) {
     const { hp, attack } = { ...defaultSettings, ...settings };
     this.name = name;
@@ -32,30 +32,42 @@ class Player {
   }
 
   playerAttackBlock() {
-    const $attackPlayerRadio = Array.from(document.querySelectorAll('.menu__attack input')).indexOf(document.querySelector('.menu__attack input:checked'));
-    const $blockPlayerRadio = Array.from(document.querySelectorAll('.menu__block input')).indexOf(document.querySelector('.menu__block input:checked'));
+    const $attackPlayerRadio = Array.from(
+      document.querySelectorAll(".menu__attack input")
+    ).indexOf(document.querySelector(".menu__attack input:checked"));
+    const $blockPlayerRadio = Array.from(
+      document.querySelectorAll(".menu__block input")
+    ).indexOf(document.querySelector(".menu__block input:checked"));
     return {
       attackPlayer: $attackPlayerRadio,
       blockPlayer: $blockPlayerRadio,
     };
   }
-  playerHealthBar ()  {
-    return $playerDamage.style.width = `${player.hp}%`
-  }
-  playerHP () {
-    if (player.playerAttackBlock().blockPlayer === rival.rivalAttackBlock().attackRival) {
+
+  playerHP() {
+    if (
+      player.playerAttackBlock().blockPlayer ===
+      rival.rivalAttackBlock().attackRival
+    ) {
       return player.hp;
-    } else {
-      return player.hp -= rival.attack;
     }
+    return (player.hp -= rival.attack);
+  }
+
+  playerHealthBar() {
+    return ($playerDamage.style.width = `${player.hp}%`);
   }
 }
 
 class Rival {
   name = null;
+
   hp = 0;
+
   attack = 0;
+
   killed = false;
+
   constructor(name, settings = null) {
     const { hp, attack } = { ...defaultSettings, ...settings };
     this.name = name;
@@ -68,46 +80,43 @@ class Rival {
     const rivalAtt = () => Math.floor(Math.random() * 5);
     return { attackRival: rivalAtt(), blockRival: rivalBlk() };
   }
-  rivalHealthBar  () {
-    return $rivalDamage.style.width = `${player.hp}%`
-  }
+
   rivalHP = () => {
-    if (rival.rivalAttackBlock().blockRival === player.playerAttackBlock().attackPlayer) {
+    if (
+      rival.rivalAttackBlock().blockRival ===
+      player.playerAttackBlock().attackPlayer
+    ) {
       return rival.hp;
-    } else {
-      return rival.hp -= player.attack;
     }
+    return (rival.hp -= player.attack);
+  };
+
+  rivalHealthBar() {
+    return ($rivalDamage.style.width = `${player.hp}%`);
   }
 }
 
-const player = new Player('player');
-const rival = new Rival('Rival');
+const player = new Player("player");
+const rival = new Rival("Rival");
 
-
-
-
-
-$go.addEventListener('click', () => {
+$go.addEventListener("click", () => {
   console.log(player.playerAttackBlock());
   console.log(rival.rivalAttackBlock());
 
-  console.log(player.playerHP())
-  console.log(rival.rivalHP())
-
+  console.log(player.playerHP());
+  console.log(rival.rivalHP());
 
   player.playerHealthBar();
   rival.rivalHealthBar();
 
-
-  if (player.hp === 0){
-    alert('player loose')
-    reload()
-  } else if (rival.hp === 0){
-    alert('Player win')
-    reload()
-  } else if (rival.hp === 0 && player.hp === 0){
-    alert('draw')
-    reload()
+  if (player.hp === 0) {
+    alert("player loose");
+    reload();
+  } else if (rival.hp === 0) {
+    alert("Player win");
+    reload();
+  } else if (rival.hp === 0 && player.hp === 0) {
+    alert("draw");
+    reload();
   }
-
 });
